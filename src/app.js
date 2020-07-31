c16fe0d6a6bdc73d81352c76ec25fed4b29ec677
Radosław Mikrut <radeon.m03@gmail.com>
2020-07-31 10:14:40 +0200

    Add cors package

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 require('./db/mongoose');
 const userRouter = require('./routers/userRouter');
 const productRouter = require('./routers/productRouter');
 const orderRouter = require('./routers/orderRouter');
 
 const app = express();
+
+const corsOptions = {
+  origin: ['http://localhost'],
+  allowedHeaders: [`Content-Type`, `Authorization`, `Access-Control-Allow-Methods`, `Access-Control-Request-Headers`],
+  credentials: true,
+  enablePreflight: true,
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 app.use(express.json());
 app.use(userRouter);
