9583d7e9bccadc705515c88ba854255020916ca1
Radosław Mikrut <radeon.m03@gmail.com>
2020-08-03 19:57:34 +0200

    Add to cookies sameSite: none and secure: true

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -8,7 +8,7 @@ router.post('/users', async (req, res) => {
   try {
     await user.save();
     const token = await user.generateAuthToken();
-    res.cookie('token', token, { httpOnly: true });
+    res.cookie('token', token, { httpOnly: true, sameSite: 'None', secure: true });
     res.status(201).send({ user });
   } catch (err) {
     res.status(400).send(err);
@@ -19,7 +19,7 @@ router.post('/users/login', async (req, res) => {
   try {
     const user = await User.findByCredentials(req.body.email, req.body.password);
     const token = await user.generateAuthToken();
-    res.cookie('token', token, { httpOnly: true });
+    res.cookie('token', token, { httpOnly: true, sameSite: 'None', secure: true });
     res.send({ user });
   } catch (err) {
     res.status(400).send(err);
